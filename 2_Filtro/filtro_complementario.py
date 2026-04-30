35684c4ffaac5048c88e335993bee8d5cd6bbd8d
sofianoguera <sofiarodrigueznog@gmail.com>
2026-04-29 20:57:06 -0400

    feat: Se agregan comentarios

--- 2_Filtro/filtro_complementario.py
+++ 2_Filtro/filtro_complementario.py
@@ -1,10 +1,31 @@
 import numpy as np, matplotlib.pyplot as plt
 
+# el programa esta simulando procesamiento de señales digitales
+# y se aplica un filtro complementario para suavizar las ondas
+
+
+# Frec 0 = 9000 Hz -> onda rapida
+# Frec 1 = 5000 Hz -> media
+# Frec 2 = 100 Hz -> lenta
+
 FREQ_0 = 9000 ; FREQ_1 = 5000; FREQ_2 = 100  # Frecuencia 1, 2 y 3
+
+# con el S_RATE tomamos 20000 muestras de cada onda, 
+# 20000/9000 = 2.22 ciclos
+# 20000/5000 = 4 ciclos
+# 20000/100 = 200 ciclos
+
+# y con el SAMPLE tomamos la duración de cada onda en segundos
 SAMPLE = 20000 ; S_RATE = 20000.0 # Samples y Tasa de Muestreo
 nMAX = 20000
 
 # Ondas....
+# tomamos la formula de sin(2*pi*frecuencia*tiempo)
+# y lo multiplicamos por un valor cualquiera (2, 3, 9) para cambiar la amplitud
+
+# i/S_RATE es el tiempo transcurrido 
+#2*pi*frecuencia es la frecuencia angular, que es la velocidad a la que oscila la onda
+# esto significa que la onda de 100hz tiene mayor amplitud 
 aW = [
     [2*np.sin(2*np.pi * FREQ_0 * i/S_RATE) for i in range(SAMPLE)],
     [3*np.sin(2*np.pi * FREQ_1 * i/S_RATE) for i in range(SAMPLE)],
@@ -12,11 +33,20 @@ aW = [
 ]
 
 #Signals...
+# creamos las señales, para la señal 1 se suma la onda 0 y la onda 1 (señal ruidosa)
+# la señal 2 es la suma de la onda 0 y la onda 2 (mezcla de rápido y lento)
+# la señal 3 es la multiplicacion de la onda 1 y la onda 2 (se distorsiona por completo)
 aS = [
     np.array(aW[0]) + np.array(aW[1]),
     np.array(aW[0]) + np.array(aW[2]),
     np.array(aW[1]) * np.array(aW[2])
 ]
+
+# es un filtro que pasa-bajos simple (suaviza la señal)
+#y[i]=α⋅x[i]+(1−α)⋅y[i−1]
+# el x[i] es la señal original
+# el y[i] es la señal filtrada
+# a(nA) es que tanto confiamos en el valor nuevo, si es mas cercano a 1 es menos suave y viceversa
 
 def Filter_Comp(aV, nA):
     # Se agregó la inicialización del arreglo aF para evitar el NameError
@@ -27,17 +57,15 @@ def Filter_Comp(aV, nA):
     return aF
 
 # Filtramos las 3 señales. Usamos nA=0.1 (suavizado fuerte) 
-# para que la línea roja se vea como en la imagen de referencia.
 nA_val = 0.1 
 aF_0 = Filter_Comp(aS[0], nA_val)
 aF_1 = Filter_Comp(aS[1], nA_val)
 aF_2 = Filter_Comp(aS[2], nA_val)
 
-# Configuramos la gráfica
-# Ponemos fondo gris claro a la figura para imitar el estilo de la captura
+# Configuramos la gráfica, 3 gráficos (uno por señal)
 fig, (ax1, ax2, ax3) = plt.subplots(3, 1, figsize=(8, 6), facecolor='lightgray')
 
-# La imagen 5 muestra exactamente el rango de 0 a 200 en el eje X
+#muestra exactamente el rango de 0 a 200 en el eje X
 VIEW_LIMIT = 200
 
 # Graficamos Signal 1
